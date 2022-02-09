e582dd514de0eada3a09c09101b82e558410e02a
perkles <maikosilvae@gmail.com>
2022-02-09 03:29:49 -0300

    aula6

--- src/com/java/HelloJava.java
+++ src/com/java/HelloJava.java
@@ -4,8 +4,15 @@ public class HelloJava {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello Java");
-
+		int idade = 10;
+		double preco = 10.5;
+		char sexo = 'M';
+		boolean temFilhos = true;
+		
+		System.out.println(idade);
+		System.out.println(preco);
+		System.out.println(sexo);
+		System.out.println(temFilhos);
 	}
 
 }
